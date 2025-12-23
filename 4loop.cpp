b542d3c25987d6ce6114bbe11480c981a99d8ca6
riajshahriar76-oss <riajshahriar76@gmail.com>
2025-12-23 10:23:23 +0600

    Add 4loop file

---
+++ 4loop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    for(int row=5;5<=6;row++){
+        for(int col=5;5<=6;col++)
+       {
+        cout<<"*";}
+        cout<<endl;
+    }
+}
